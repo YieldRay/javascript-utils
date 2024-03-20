6740fa6f31edec3aeaadafcae21700b40650a2be
YieldRay <yieldray@outlook.com>
2024-03-20 21:58:26 +0800

    add comment

--- activitypub/key_use.ts
+++ activitypub/key_use.ts
@@ -1,17 +1,18 @@
 import { encodeBase64, decodeBase64 } from "./base64.ts";
 import { ALGORITHM } from "./key_to_text.ts";
 
-export async function signDataAsBase64(key: CryptoKey, data: BufferSource | string) {
-    return encodeBase64(await signData(key, data));
-}
-
+/**
+ * @returns signature
+ */
 export async function signData(key: CryptoKey, data: BufferSource | string): Promise<ArrayBuffer> {
     return await crypto.subtle.sign(ALGORITHM, key, typeof data === "object" ? data : new TextEncoder().encode(data));
 }
 
-export async function verifyDataFromBase64(key: CryptoKey, data: string, signature: string): Promise<boolean> {
-    const decodedSignature = decodeBase64(signature);
-    return verifyData(key, data, decodedSignature);
+/**
+ * @returns signature as base64 string
+ */
+export async function signDataAsBase64(key: CryptoKey, data: BufferSource | string) {
+    return encodeBase64(await signData(key, data));
 }
 
 export async function verifyData(
@@ -25,4 +26,16 @@ export async function verifyData(
         signature,
         typeof data === "object" ? data : new TextEncoder().encode(data)
     );
+}
+
+/**
+ * note that it's the signature in base64, not the data
+ */
+export async function verifyDataFromBase64(
+    key: CryptoKey,
+    data: BufferSource | string,
+    signatureInBase64: string
+): Promise<boolean> {
+    const signature = decodeBase64(signatureInBase64);
+    return verifyData(key, data, signature);
 }
